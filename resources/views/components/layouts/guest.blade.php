a928d777a45d8cd4e15f381353428ff01cb63fcc
Alhilal Anwar <alhilalanwar07@gmail.com>
2026-04-25 20:26:36 +0800

    update excel dan pdf

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -1,10 +1,25 @@
+@php
+    $profil = \Illuminate\Support\Facades\Cache::remember(
+        'site:profil:v1',
+        now()->addMinutes(10),
+        fn () => \App\Models\ProfilSekolah::first()
+    );
+    $namaSekolah = $profil->nama_sekolah ?? 'SMK Negeri 1 Kolaka';
+@endphp
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ $title ?? 'Login - SMK Negeri 1 Kolaka' }}</title>
+    <title>{{ $title ?? 'Login - ' . $namaSekolah }}</title>
+
+    @if($profil && $profil->favicon_path)
+    <link rel="icon" href="{{ Storage::url($profil->favicon_path) }}">
+    @else
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    @endif
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
